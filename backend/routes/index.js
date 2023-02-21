eff0a43cc37d16e6b4b56d7c5edf8f22b8f2fa73
anti-epic <donahoe.chase@gmail.com>
2023-02-21 12:00:23 -0600

    setup api router for bbackend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+
+
+
+//middlewear directing all /api calls to correct folder
+router.use('/api', apiRouter);
+
+
 
 // Add a XSRF-TOKEN cookie in dev NOT production once finished
 router.get("/api/csrf/restore", (req, res) => {
