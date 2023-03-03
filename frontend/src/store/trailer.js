235224a2aae2035e69d816cdb964fc5be77ae5fb
anti-epic <donahoe.chase@gmail.com>
2023-03-02 20:51:12 -0600

    added css to moviepage for trailer, added trailers component

---
+++ frontend/src/store/trailer.js
@@ -0,0 +1,55 @@
+import {csrfFetch} from './csrf'
+
+const LOAD_TRAILER= '/trailer/load'
+
+
+
+export const getTrailerThunk = (movieId) => async dispatch => {
+    const response = await csrfFetch(`https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=61e69f523be7c038d620f35a02dd450e&language=en-US`)
+if(response.ok){
+    const trailer = await response.json()
+    dispatch(getTrailer(trailer))
+}
+}
+
+
+
+
+
+const getTrailer = (trailer) =>{
+    return {type: LOAD_TRAILER,
+        trailer
+    }
+}
+
+
+
+
+const initialState = {
+};
+
+
+
+
+const trailerReducer = (state = initialState, action) => {
+    switch(action.type){
+        case LOAD_TRAILER:
+            let loadTrailerState = {
+            }
+            console.log(action.trailer.results,'lol')
+            if(action.trailer.results){
+                action.trailer.results.forEach(video => {
+                    if(video.site === "YouTube" && video.type === 'Trailer'){
+                        loadTrailerState[video.id] = video
+                    }
+                })
+            }
+
+            return loadTrailerState
+        default:
+            return state
+    }
+}
+
+
+export default trailerReducer
