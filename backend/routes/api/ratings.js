cdec27dc72cdc89034af6e80498ae3238e2b3925
anti-epic <donahoe.chase@gmail.com>
2023-03-29 11:14:50 -0500

    added try catch block to post ratings

--- backend/routes/api/ratings.js
+++ backend/routes/api/ratings.js
@@ -39,33 +39,75 @@ router.get('/', requireAuth, async(req,res,next) =>{
 
 
 router.post('/:movieId', requireAuth, async (req, res, next) => {
-  const {movieId} = req.params;
-  const {rating} = req.body;
-  console.log(typeof req.user.id)
-  const user = await User.findByPk(req.user.id);
-  const movie = await Movie.findByPk(movieId);
-  if (! movie) {
-      res.statusCode = 404;
-      return res.json({"message": "Movie couldn't be found", "statusCode": res.statusCode})
+//   const {movieId} = req.params;
+//   const {rating} = req.body;
+//   console.log(typeof req.user.id)
+//   const user = await User.findByPk(req.user.id);
+//   const movie = await Movie.findByPk(movieId);
+//   if (! movie) {
+//       res.statusCode = 404;
+//       return res.json({"message": "Movie couldn't be found", "statusCode": res.statusCode})
+//   }
+
+//   let userRatings = await Rating.findAll({
+//     attributes: ['id', 'userId', 'movieId', 'stars', 'createdAt', 'updatedAt'],
+//     where: {userId:req.user.id}
+// })
+// for(let i = 0; i < userRatings.length; i++){
+//   if(userRatings[i].dataValues.movieId == movieId){
+//     userRatings[i].destroy()
+//   }
+// }
+//   const newRating = await Rating.create({
+//   stars: rating,
+//   movieId: movieId,
+//   userId: req.user.id,
+//   UserId: user.id // use the user's id as the foreign key value
+// });
+//   res.statusCode = 201;
+//   return res.json({"message" : "new rating added"})
+
+
+
+  const rating = req.body.rating;
+  const movieId = req.params.movieId;
+
+
+
+  if (!req.user || !req.user.id) {
+    return res.status(401).json({ message: 'User is not authenticated' });
   }
 
-  let userRatings = await Rating.findAll({
+  try {
+      let userRatings = await Rating.findAll({
     attributes: ['id', 'userId', 'movieId', 'stars', 'createdAt', 'updatedAt'],
     where: {userId:req.user.id}
 })
-for(let i = 0; i < userRatings.length; i++){
+    for(let i = 0; i < userRatings.length; i++){
   if(userRatings[i].dataValues.movieId == movieId){
     userRatings[i].destroy()
   }
 }
-  const newRating = await Rating.create({
-  stars: rating,
-  movieId: movieId,
-  userId: req.user.id,
-  UserId: user.id // use the user's id as the foreign key value
-});
-  res.statusCode = 201;
-  return res.json({"message" : "new rating added"})
+    const newRating = await Rating.create({
+      stars: rating,
+      movieId,
+      userId: req.user.id
+    });
+    return res.json(newRating);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+
+
+
+
+
+
+
+
+
+
 });
 
 
